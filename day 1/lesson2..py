c6d5d3d27279fdc174b60ddfdb2869d91cdfd45d
nikolozi124525 <kupatadzenika110@gmail.com>
2024-02-04 06:58:34 -0800

    first commit

---
+++ day 1/lesson2..py
@@ -0,0 +1,27 @@
+name = "nika"
+
+print(name)
+
+surname = "kupatadze"
+#name არის ცვლადი
+# = არის ცვლადისთვის მიმნიჭებელი სიმბოლო
+# "nika"  არის ცვლადისთვის მნიოშვნელობა
+#print ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "nika" #ეს არის str (string) ტიპის ცვლადი
+age = 22# ეს არის int (integer) მთელი  რიცხვი
+height = 184.5 #ეს არის float ტიპის ცვლადი ( ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+knows_programming = True # true end false
+is_ugly = false #snakecase( უბრალოდ წერის სტილი სტანდარტულად)                
+isUgly = False  # ჯავასცრიპტული camelcase
+# სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+print(name +" "+ surname)
+# print( name + age)
+
+print(type(age))
+
+
+
+
+
