fbf7fb2a2a1e7417e9824ba71ccff0b56836768a
nikolozi124525 <kupatadzenika110@gmail.com>
2024-02-09 05:51:15 -0800

    comimt

---
+++ day 2/goa2.py
@@ -0,0 +1,12 @@
+name = "nika"
+last_name = "kupatadze."
+age = "i am 14 years old."
+heigit = "i am 165.4cm tall." 
+about_my = "i love football."
+
+
+
+
+print( name + " " + last_name + " " + age + " " + heigit + " " + about_my   )    
+
+
