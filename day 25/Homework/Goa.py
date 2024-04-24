6c0075b3208767f18383e3f36bfd2dfee28be25f
nikolozi124525 <kupatadzenika110@gmail.com>
2024-04-24 11:45:20 -0700

    eighth commit

---
+++ day 25/Homework/Goa.py
@@ -0,0 +1,8 @@
+def add(name,age):
+    print('heppy birthday{name}')
+    print('you are{age}years old')
+    print('happy birthday')
+
+add('nika kupatadze',14)
+add('dato wiqridze',14)
+add('luka kirkitadze',13)
